79ccb317d66afeb9b3588cc0452196fe12ec5481
ambatineeraj <ambatineerajakshulu>
2021-11-21 07:09:10 +0530

    changed hompage

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Dashboard(){
     return <div>
-              <h1>Welcome to Reactbook Dashboard</h1>
+              <h1>Welcome to Reactbook Home page</h1>
     </div>
 }
 export default Dashboard
